056e5500010afc19c9731b2a4b7d1b0057d52624
samirasharma <samira.sharma@outlook.com>
2017-09-17 18:42:25 +0545

    simple splash screen designed

--- Splash.js
+++ Splash.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
-import {View, Text } from 'react-native';
+import {View, Text, StyleSheet } from 'react-native';
 export default class  extends Component{
 	render(){
 		return(
-			<View style ={{backgroundColor:'red' flex : 1}}>
+			<View style ={styles.wrapper}>
+			<View>
+			<Text style = {styles.title}> Reminder Application</Text>
+			</View>
+			<View >
+			<Text style = {styles.subtitle}> FirstReact native application </Text>
+			</View>
 			</View>
 			);
+		}
 	}
-}
+	const styles = StyleSheet.create({
+		wrapper : {
+			backgroundColor:'green', 
+			flex : 1, 
+			alignItems:'center', 
+			justifyContent:'center'},
+		title :{
+			color :'white', 
+			fontSize :35,
+			fontWeight  :'bold'
+		}, 
+		subtitle:{
+			color :'white', 
+			fontSize: 16
+		}
+	});
